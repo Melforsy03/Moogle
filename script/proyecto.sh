3df99f1690f9fb76a8eb77be1a577c5be6a5d9b5
melani <forsythemelani@gmail.com>
2023-07-25 12:18:55 -0400

    commit

--- script/proyecto.sh
+++ script/proyecto.sh
@@ -1,60 +1,70 @@
 
 #!/bin/bash
 
-if [$1 == "run"] ; then
+if [ $1 == "run" ] ; then
+
 cd ..
 dotnet watch run --project MoogleServer
-fi
 
-if [$1 == "report"] ; then
+elif [ $1 == "report" ] ; then
+
 cd Informe
 
 latexmk -pdf Informe.tex
 
-rm Informe.aux Informe.fdb_latexmk Informe.fls Informe.log
-
-xdg-open Informe.pdf
-fi
-if [$1 == "show_report"] ; then 
+elif [ $1 == "slides" ] ; then 
 
 cd Presentacion
 
 latexmk -pdf Presentacion.tex
 
-rm Presentacion.aux Presentacion.fdb_latexmk Presentacion.fls Presentacion.log
+
+elif [ $1 == "clean" ] ; then 
+
+rm Informe.aux Informe.fdb_latexmk Informe.fls Informe.log Informe.pdf
+
+rm Presentacion.aux Presentacion.fdb_latexmk Presentacion.fls Informe.log Presentacion.pdf
+
+rm -r obj 
+
+rm -r bin
+
+
+elif [ $1 == "show_slides" ] ; then 
+
+cd Presentacion
+
+#si pasa un parametro del visualizador 
+if [ -n "$2" ] ; then 
+
+ "$2" Presentacion.pdf
+
+else 
+#visualizador por defecto
 
 xdg-open Presentacion.pdf
-fi
-if [$1 == "clean"] ; then 
-#compilar el programa de c#
-
-mcs programa.cs
-
-#Ejecutar el progrma 
-
-mono programa.exe
-
-#limpiar archivos generados 
-
-rm programa.exe programa.mdb
 
 fi
-if [$1 == "show_slides"] ; then
 
-if [ -n "$1" ] ; then 
+ 
+ #show report
 
-visor ="$1"
+ else 
+
+elif [ $1 == "show_report" ] ; then 
+
+cd Informe
+
+if [ -n "$2" ] ; then 
+
+"$2" Informe.pdf
 
 else 
 
-visor="xdg-open"
+xdg-open Informe.pdf
+
 fi
-directory_script ="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /deev/null/ && pwd )"
 
-archivo_pdf ="directory_script/Presentacion.pdf"
-
-$visor "archivo_pdf"
- 
 fi
 
 
