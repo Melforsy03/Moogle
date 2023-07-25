6c5f452e1b7297c40007da21943264be630cb60e
melani <forsythemelani@gmail.com>
2023-07-25 18:25:41 -0400

    commit

--- script/proyecto.sh
+++ script/proyecto.sh
@@ -31,44 +31,59 @@ cd Informe
 cd ..
 cd Presentacion
 
- rm Presentacion.aux Presentacion.fdb_latexmk Presentacion.fls Informe.log Presentacion.pdf Presentacion.txt
+ rm Presentacion.aux Presentacion.fdb_latexmk Presentacion.fls Presentacion.log Presentacion.pdf 
 
 cd ..
  rm -r obj 
 
  rm -r bin
+
 fi
 
 if [ "$A" == "show slides" ] ;
+
 then 
+
  cd Presentacion
+
  read B
  #si pasa un parametro del visualizador 
- if [ -n "$B" ] ;
+ if [  -n "$B" ] ;
+
  then 
- "$B" Presentacion.pdf
 
- else 
- #visualizador por defecto
+ $B Presentacion.pdf
 
- xdg-open Presentacion.pdf
+else
 
- fi
- #show report
- cd ..
-fi
+ start Presentacion.pdf
+
  
-if [ "$A" == "show report" ] ;
- then 
- read B
- cd Informe
- if [ -n "$B" ] ; 
- then 
- "$B" Informe.pdf
+ #show report
+ 
 
- else 
- xdg-open Informe.pdf
- fi
+fi
+cd ..
+fi
+
+if [ "$A" == "show report" ] ;
+
+ then 
+
+ cd Informe
+
+ read B
+ if [ -n "$B" ] ; 
+
+ then 
+
+ $B Informe.pdf
+
+else 
+
+ start Informe.pdf
+
+fi
  cd ..
 fi
 
