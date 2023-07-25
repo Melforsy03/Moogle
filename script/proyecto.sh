ebf180f46457fc2d35f1b241a3c74f7a6e7a3225
melani <forsythemelani@gmail.com>
2023-07-25 16:19:59 -0400

    commit

--- script/proyecto.sh
+++ script/proyecto.sh
@@ -1,68 +1,75 @@
 
 #!/bin/bash
 
-if [ $1=="run" ] ; then
+read A
 
 cd ..
-dotnet watch run --project MoogleServer
 
-elif [ $1=="report" ] ; then
-
-cd Informe
-
-latexmk -pdf Informe.tex
-
-elif [ $1=="slides" ] ; then 
-
-cd Presentacion
-
-latexmk -pdf Presentacion.tex
-
-
-elif [ $1=="clean" ] ; then 
-
-rm Informe.aux Informe.fdb_latexmk Informe.fls Informe.log Informe.pdf
-
-rm Presentacion.aux Presentacion.fdb_latexmk Presentacion.fls Informe.log Presentacion.pdf
-
-rm -r obj 
-
-rm -r bin
-
-
-elif [ $1=="show_slides" ] ; then 
-
-cd Presentacion
-
-#si pasa un parametro del visualizador 
-if [ -n "$2" ] ; then 
-
- "$2" Presentacion.pdf
-
-else 
-#visualizador por defecto
-
-xdg-open Presentacion.pdf
-
+if [ "$A" == "run" ] ;
+ then
+   dotnet watch run --project MoogleServer
 fi
- #show report
+
+if [ "$A" == "report" ] ;
+then
+ cd Informe
+ latexmk -pdf Informe.tex
+  cd ..
+fi
+
+if [ "$A" == "slides" ] ; 
+then 
+ cd Presentacion
+ latexmk -pdf Presentacion.tex
+  cd ..
+fi
+
+if [ "$A" == "clean" ] ; 
+then 
+cd Informe 
+ rm Informe.aux Informe.fdb_latexmk Informe.fls Informe.log Informe.pdf
+cd ..
+cd Presentacion
+
+ rm Presentacion.aux Presentacion.fdb_latexmk Presentacion.fls Informe.log Presentacion.pdf Presentacion.txt
+
+cd ..
+ rm -r obj 
+
+ rm -r bin
+fi
+
+if [ "$A" == "show slides" ] ;
+then 
+ cd Presentacion
+ read B
+ #si pasa un parametro del visualizador 
+ if [ -n "$B" ] ;
+ then 
+ "$B" Presentacion.pdf
 
  else 
+ #visualizador por defecto
 
-if [ $1=="show_report" ] ; then 
+ xdg-open Presentacion.pdf
 
-cd Informe
+ fi
+ #show report
+ cd ..
+fi
+ 
+if [ "$A" == "show report" ] ;
+ then 
+ read B
+ cd Informe
+ if [ -n "$B" ] ; 
+ then 
+ "$B" Informe.pdf
 
-if [ -n "$2" ] ; then 
-
-"$2" Informe.pdf
-
-else 
-
-xdg-open Informe.pdf
-
+ else 
+ xdg-open Informe.pdf
+ fi
+ cd ..
 fi
 
-fi
 
-fi
